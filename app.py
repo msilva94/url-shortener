43e2816d9ebe5681dc4277c3aaabf449f0ffe5e3
Miguel Silva <71125410+msilva94@users.noreply.github.com>
2023-07-14 16:40:36 +0100

    Added command to create database

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from url_shortener import app
+from url_shortener import app, db
+
+@app.cli.command("create_db")
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 if __name__ == "__main__":
     app.run()
