dee8ac2b810ae3e63585b968755637bcb5d15cc2
Miguel Silva <71125410+msilva94@users.noreply.github.com>
2023-07-17 17:01:32 +0100

    Changed the way unique hashes are generated

--- url_shortener/utils.py
+++ url_shortener/utils.py
@@ -1,7 +1,5 @@
 from uuid import uuid4
 
-from sqlalchemy.exc import IntegrityError
-
 from url_shortener import db
 
 from .models import URL
@@ -11,17 +9,17 @@ def generate_hash():
     return uuid4().hex[:10]
 
 
+def generate_unique_hash():
+    hash = generate_hash()
+    while URL.query.filter_by(hash=hash).first() is not None:
+        hash = generate_hash()
+    return hash
+
+
 def get_or_create_url(original_url):
     instance = URL.query.filter_by(original_url=original_url).first()
     if not instance:
-        created = False
-        while not created:
-            instance = URL(original_url=original_url, hash=generate_hash())
-            try:
-                db.session.add(instance)
-                db.session.commit()
-            except IntegrityError:
-                db.session.rollback()
-            else:
-                created = True
+        instance = URL(original_url=original_url, hash=generate_unique_hash())
+        db.session.add(instance)
+        db.session.commit()
     return instance
